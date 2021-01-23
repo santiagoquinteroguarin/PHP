09111399df2fce7af41bac7153fcb442cf2b2a96
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-23 11:04:17 -0500

    increment and decrement in php

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+// Incremento
+$numberA = 10;
+$numberA++;
 
+echo($numberA);
+
+// Decremento
+$numberB = 20;
+$numberB--;
+
+echo($numberB);
+
+$numberC = 5;
+$numberC += 2;
+
+echo($numberC);
+
+$numberD = 2;
+$numberD *= 3;
+
+echo($numberD);
 
 
 include 'includes/footer.php';
