adeaf4dd504724f1f185a46e9ee604377117268a
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-22 19:51:30 -0500

    Introduction to PHP - Hello World in PHP

---
+++ 01-hola-mundo.php
@@ -0,0 +1,18 @@
+<!-- Formas de imprimir en php -->
+<?php 
+// incluir un archivo externo
+include('../Introduccion-PHP/includes/header.php');
+
+// ? echo: es la forma más común para imprimir un mensaje
+echo 'Hola mundo';
+
+// ? var_dump: imprime el mensaje pero además nos da mas información como cuantos caracteres hay en esa cadena
+var_dump('Hola Mundo!');
+
+// ? print: imprime el texto
+print('Hola Mundo!');
+
+// ? print_r: es muy utilizado en arreglos para imprimir  
+print_r('Hola mundo');
+
+include('../Introduccion-PHP/includes/footer.php');
