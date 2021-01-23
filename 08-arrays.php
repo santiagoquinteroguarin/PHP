51a44304f2bb47fdf123ab5e820484fce0ec62be
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-23 12:44:33 -0500

    arrays in php

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,22 @@
 <?php include 'includes/header.php';
 
+// ? Arrays
 
+$carrito = ['tablet','phone','laptop','tv'];
 
+// * pre --> util para ver los contenidos de un array
+echo '<pre>';
+    var_dump($carrito);
+echo '<pre>';
+
+// * añadir un nuevo item al final de array
+array_push($carrito, 'smartwatch');
+
+// * añadir un nuevo item al inicio del array
+array_unshift($carrito, 'playstation');
+
+echo '<pre>';
+    var_dump($carrito);
+echo '<pre>';
 
 include 'includes/footer.php';
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+// ? arreglos asociativos
 
+$cliente = [
+    'name' => 'Santiago',
+    'lastname' => 'Quintero Guarin',
+    'data' => [
+        'tipo' => 'premium',
+        'dinero' => 200
+    ],
+];
+
+
+echo($cliente['name']);
+
+$cliente['age'] = 23;
+
+echo '<pre>';
+    var_dump($cliente);
+echo '<pre>';
 
 
 include 'includes/footer.php';
