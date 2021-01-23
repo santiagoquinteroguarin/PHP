1ef4d0472f37ee57ce1b2a8e6fa42b84f40afef1
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-22 20:25:42 -0500

    Numbers and operators in php

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,25 @@
 <?php include 'includes/header.php';
 
+// ? Números y operadores en php
 
+$numberA = 5;
+$numberB = 10;
+
+// * Suma
+$resultado = $numberA + $numberB;
+
+// * Resta
+$resultado = $numberA - $numberB;
+
+// * Multiplicación
+$resultado = $numberA * $numberB;
+
+// * División
+$resultado = $numberA / $numberB;
+
+// * Multiplicar un numero cierta cantidad de veces n°..
+$resultado = 2 ** 3;
+echo($resultado);
 
 
 include 'includes/footer.php';
