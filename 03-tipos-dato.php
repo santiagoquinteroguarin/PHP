ca64d3fdf1e6f96312daa63e8da2d55d45d8b26c
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-22 20:17:08 -0500

    data types in php

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+// ? tipos de datos en php
 
+// * Booleanos
+$on = true;
+$of = false;
+
+var_dump($on, $of);
+
+// * Enteros
+$age = 23;
+var_dump($age);
+
+// * Flotantes
+$price = 20.000;
+var_dump($price);
+
+// * Strings
+$name = 'Santiago';
+var_dump($name);
+
+// * Arrays
+$colors = ['red','blue','orange'];
+var_dump($colors);
 
 
 include 'includes/footer.php';
