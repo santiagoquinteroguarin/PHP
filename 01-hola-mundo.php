fc9637ed4c246ce3dbc6cab670bbaeda032fbf86
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-22 20:08:01 -0500

    How create variable in php

--- 01-hola-mundo.php
+++ 01-hola-mundo.php
@@ -6,7 +6,7 @@ include('../Introduccion-PHP/includes/header.php');
 // ? echo: es la forma más común para imprimir un mensaje
 echo 'Hola mundo';
 
-// ? var_dump: imprime el mensaje pero además nos da mas información como cuantos caracteres hay en esa cadena
+// ? var_dump: imprime el mensaje pero además nos da mas información como cuantos caracteres hay en esa cadena y el tipo de dato de la variable
 var_dump('Hola Mundo!');
 
 // ? print: imprime el texto
--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,22 @@
 <?php include 'includes/header.php';
 
+// ? Variables en PHP
+
+// * $variables: esta funciona como un let en js tienen la misma funcionalidad, se puede reasignar un valor etc...
+$name = 'Santiago';
+
+var_dump($name);
+echo($name);
 
 
+// * crear una variable constante en php y para mostrarla no se necesita agregar el signo de dolar
+define('constante','Soy una variable constante');
+
+echo constante;
+
+// ? otra forma de crear una constante pero no muy utilizada en php es como en js
+const lastName = 'Quintero Guarin';
+
+echo lastName;
 
 include 'includes/footer.php';
